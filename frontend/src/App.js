d173bfd24945ff90c69019ac923c507fc9b3b4a1
Amritanshu rai <amritanshurai13@gmail.com>
2020-12-05 07:26:40 +0530

    added product images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+
+import { Container } from 'react-bootstrap';
+
 function App() {
-  return <> New project </>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
