d403012c64a199a24ab8e4172789a0792702b958
Amritanshu rai <amritanshurai13@gmail.com>
2020-12-08 04:09:52 +0530

    minor bug in fetching fixed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -19,12 +19,10 @@ router.get(
 
     const product = await Product.findById(req.params.id);
     if (!product) {
-      return res.status(404).json({
-        message: 'Product not found',
-      });
+      res.status(404);
+      throw new Error('Product not found');
     }
-    res.status(404);
-    throw new Error('Product not found');
+    return res.status(200).json(product);
   })
 );
 
