b4eecfae96214c9260d586ba5cee1800c0aa2458
Amritanshu rai <amritanshurai13@gmail.com>
2020-12-10 20:59:37 +0530

    user login implemented

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -26,6 +26,10 @@ const userSchema = mongoose.Schema(
   }
 );
 
+//method runs after the call to DB i guess
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,20 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
 const productRoutes = require('./routes/productRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
 
 dotenv.config();
 
 connectDB();
 const app = express();
-
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('It live bitches');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT;
