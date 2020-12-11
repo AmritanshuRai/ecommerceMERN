2f0b2c9428717375bcdde6766c43a0b37be6d89a
Amritanshu rai <amritanshurai13@gmail.com>
2020-12-11 23:18:09 +0530

    user login action, reducers FE

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,28 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+} from '../constants/userConstant';
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+    const url = `/api/users/login`;
+    let response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    if (data.message) {
+      throw new Error(data.message);
+    }
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({ type: USER_LOGIN_FAIL, error });
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,18 +6,24 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems');
 const cartItems = cartItemsFromStorage ? JSON.parse(cartItemsFromStorage) : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo');
+const userInfo = userInfoFromStorage ? JSON.parse(userInfoFromStorage) : null;
+
 const initialState = {
   cart: { cartItems },
+  userLogin: { userInfo },
 };
 const middleware = [thunk];
 const store = createStore(
