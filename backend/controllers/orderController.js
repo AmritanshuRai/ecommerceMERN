72c6866e9141ca02362976c97991c9820abfe8f7
Amritanshu rai <amritanshurai13@gmail.com>
2020-12-14 17:59:24 +0530

    update order route created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,5 +43,27 @@ const getOrderById = asyncHandler(async (req, res) => {
     throw new Error('Order not found');
   }
 });
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 exports.addOrderItems = addOrderItems;
 exports.getOrderById = getOrderById;
+exports.updateOrderToPaid = updateOrderToPaid;
